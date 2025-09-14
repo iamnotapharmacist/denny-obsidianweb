09bada520bcb3ea6088217148818c1607762ecc7
iamnotapharmacist <dennyrianto0@gmail.com>
2025-09-15 01:01:26 +0700

    Quartz sync: Sep 15, 2025, 1:01 AM

--- content/index.md
+++ content/index.md
@@ -1,6 +1,7 @@
 ---
-title: Welcome to Quartz
+title: Denny's Notebook "online"
 ---
 
-This is a blank Quartz installation.
-See the [documentation](https://quartz.jzhao.xyz) for how to get started.
+Welcome to my digital notebook!.
+
+This is my space to capture my thoughts, ideas, and everything that inspires me. Here you'll find a collection of my projects, notes, and a record of my personal journey.
--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Denny's Journal: A Collection of Thoughts & Ideas",
+    pageTitle: "",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "iamnotapharmacist.github.io/obsidianweb",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
