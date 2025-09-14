03d77d723ab7b52ea88aa3ac87a9edca81371c6d
iamnotapharmacist <dennyrianto0@gmail.com>
2025-09-15 00:44:46 +0700

    Quartz sync: Sep 15, 2025, 12:44 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Denny's Journal: A Collection of Thoughts & Ideas",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
